9029f0e0da4153e989f8def900849f164517d265
sebas1541 <sebastian.ca0102@gmail.com>
2025-03-10 13:02:11 -0500

    fix: unit tests for vehicle service

--- src/main/java/com/translogistics/fleetmanagmentsystem/model/Vehicle.java
+++ src/main/java/com/translogistics/fleetmanagmentsystem/model/Vehicle.java
@@ -9,8 +9,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "vehicles")
-@Getter
-@Setter
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vehicle extends BaseEntity {
@@ -49,4 +48,83 @@ public class Vehicle extends BaseEntity {
     @OneToMany(mappedBy = "vehicle")
     private List<Maintenance> maintenances;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+    public VehicleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(VehicleStatus status) {
+        this.status = status;
+    }
+
+    public List<DriverVehicleAssignment> getDriverAssignments() {
+        return driverAssignments;
+    }
+
+    public void setDriverAssignments(List<DriverVehicleAssignment> driverAssignments) {
+        this.driverAssignments = driverAssignments;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public List<Trip> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(List<Trip> trips) {
+        this.trips = trips;
+    }
+
+    public List<Maintenance> getMaintenances() {
+        return maintenances;
+    }
+
+    public void setMaintenances(List<Maintenance> maintenances) {
+        this.maintenances = maintenances;
+    }
 }
