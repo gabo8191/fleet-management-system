02bed355e697b909528e51287f9d24bbde0a3032
sebas1541 <sebastian.ca0102@gmail.com>
2025-03-10 12:47:25 -0500

    fix: created at and updated at values that prevented app to run

--- src/main/java/com/translogistics/fleetmanagmentsystem/FleetManagmentSystemApplication.java
+++ src/main/java/com/translogistics/fleetmanagmentsystem/FleetManagmentSystemApplication.java
@@ -2,8 +2,10 @@ package com.translogistics.fleetmanagmentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FleetManagmentSystemApplication {
 
     public static void main(String[] args) {
