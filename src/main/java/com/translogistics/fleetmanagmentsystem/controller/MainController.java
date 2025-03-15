6c8093ac8dc26c85e643ec4393c24f09e4f68255
Gabriel Castillo <gabriel.castillo03@uptc.edu.co>
2025-03-15 17:06:24 -0500

    fix: change the path

--- src/main/java/com/translogistics/fleetmanagmentsystem/controller/MainController.java
+++ src/main/java/com/translogistics/fleetmanagmentsystem/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String home() {
         return "¡Hola! La aplicación está funcionando";
     }
